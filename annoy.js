4432815bef5daa275e74018c6ca4e033640c6f72
yuqi <yuqi.yang@mail.mcgill.ca>
2019-11-27 13:05:13 -0500

    whileloop

---
+++ annoy.js
@@ -0,0 +1,20 @@
+// alert("connect");
+
+
+var answer=prompt("are we there yet?");
+// if(answer==="yes"){
+// 	alert("YAY, We made it!")
+// }else{
+// 	var answer=prompt("are we there yet?")
+// }
+
+// while(answer!=="yes" && anser!=="yey"){
+// 	var answer=prompt("are we there yet?")
+// }
+
+// alert("YAY, WE MADE IT");
+
+while(answer.indexOf("yes") ==-1){
+	var answer=prompt("are we there yet?")
+}
+alert("YAY, WE MADE IT");
