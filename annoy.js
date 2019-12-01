87f00389f3e8ebd508cf2a30caf952143a020194
yuqi <yuqi.yang@mail.mcgill.ca>
2019-12-01 14:51:00 -0500

    modifiy code

--- annoy.js
+++ annoy.js
@@ -18,3 +18,25 @@ while(answer.indexOf("yes") ==-1){
 	var answer=prompt("are we there yet?")
 }
 alert("YAY, WE MADE IT");
+
+
+for(var count=-10; count<=19; count++){
+	console.log(count);
+}
+
+for(var count=10; count<=40; count+=2){
+	console.log(count);
+}
+
+
+for(var count=300; count<=333; count++){
+	if(count%2!==0){
+		console.log(count);
+	}
+}
+
+for(var count=5; count<=50; count++){
+	if(count%3==0&&count%5==0){
+		console.log(count);
+	}
+}
